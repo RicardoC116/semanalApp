ee0a955d73e17827a855a73d2e48052d6cec6ce1
RicardoC116 <contrerasr1136@gmail.com>
2024-10-28 11:45:09 -0600

    pre alfa v1 conexion

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,42 @@
-// HomeScreen.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button, Alert, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+const jwtDecode = require("jwt-decode"); // Importación
 
 export default function HomeScreen({ onLogout }) {
+  const [cobrador, setCobrador] = useState({ id: null, name: "" });
+
+  useEffect(() => {
+    const fetchCobradorData = async () => {
+      try {
+        const token = await AsyncStorage.getItem("token");
+        console.log("*********Token:", token, "********"); // Verificar el token
+        if (token) {
+          try {
+            const decoded = jwtDecode(token);
+            console.log("********* Decoded Token:", decoded, "********");  // Verificar el token
+
+            const id = decoded.id;
+            const name = decoded.name;
+            if (id && name) {
+              setCobrador({ id, name });
+            } else {
+              console.warn("ID o nombre no encontrados en el token.");
+            }
+          } catch (error) {
+            console.error("Error al decodificar el token:", error);
+          }
+        }
+      } catch (error) {
+        console.error("Error al obtener datos del cobrador:", error);
+      }
+    };
+
+    fetchCobradorData();
+  }, []);
+
   const handleLogout = async () => {
     try {
-      // Eliminar token de AsyncStorage
       await AsyncStorage.removeItem("token");
       Alert.alert("Sesión Cerrada", "Has cerrado la sesión correctamente.");
       onLogout();
@@ -18,7 +48,9 @@ export default function HomeScreen({ onLogout }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>¡Bienvenido a la aplicación!</Text>
+      <Text style={styles.welcomeText}>
+        ¡Bienvenido, {cobrador.name}! (ID: {cobrador.id})
+      </Text>
       <Button title="Cerrar Sesión" onPress={handleLogout} color="#dc3545" />
     </View>
   );
