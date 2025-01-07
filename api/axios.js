c9b9460263c2269276695a8024678f946467d829
RicardoC116 <contrerasr1136@gmail.com>
2025-01-07 16:28:23 -0600

    Beta v1

--- api/axios.js
+++ api/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.80.235:3000/api",
+  // casa
+  // baseURL: "http://192.168.80.235:3000/api",
+
+  // Para test
+  // baseURL: "http://192.168.1.110:3000/api",
+
+  baseURL: "https://cobrobackend2-production.up.railway.app/api",
 });
 
 export default api;
